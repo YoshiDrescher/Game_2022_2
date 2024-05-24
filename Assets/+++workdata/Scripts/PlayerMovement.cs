11b29d4df3426a3d55f283da5cfe52ffce08b37c
DSSCHULE\schueler <drescher@designschule.de>
2024-05-24 10:48:40 +0200

    Added a Camera to the player

--- Assets/+++workdata/Scripts/PlayerMovement.cs
+++ Assets/+++workdata/Scripts/PlayerMovement.cs
@@ -22,17 +22,54 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask layerGroundCheck;
 
     [SerializeField] private float maxAngleSlope = 20f;
+    [SerializeField] Transform transformCameraFollow;
+    [SerializeField] float rotateSensivity = 1f;
+    private float cameraPitch = 0f;
+    private float cameraRoll = 0f;
+    [SerializeField] float maxCameraPitch = 60f;
+    [SerializeField] bool invertCameraPitch = false;
+
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        playerRigidbody.freezeRotation = true;
         movingSpeed = defaultSpeed;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+        if (invertCameraPitch)
+        {
+            cameraPitch = cameraPitch - mouseDelta.y * rotateSensivity;
+        }
+        else
+        {
+            cameraPitch = cameraPitch + mouseDelta.y * rotateSensivity;
+        }
+
+        cameraPitch = Mathf.Clamp(cameraPitch, -maxCameraPitch, maxCameraPitch);
+
+        cameraRoll = cameraRoll + mouseDelta.x * rotateSensivity;
+
+        transformCameraFollow.localEulerAngles = new Vector3(cameraPitch, cameraRoll, 0);
     }
 
      private void FixedUpdate()
     {
         if (SlopeCheck())
         {
+            Debug.Log("Rotation y CameraFollowObject: " + transformCameraFollow.eulerAngles.y);
+            Debug.Log("current inputVector " + inputVector);
+
+            Vector3 movementDirection = Quaternion.AngleAxis(transformCameraFollow.eulerAngles.y, Vector3.up) * inputVector;
+
+            Debug.Log("rotated MovementVector: " + movementDirection);
+
             playerRigidbody.velocity = new Vector3(inputValue.x * playerSpeed, playerRigidbody.velocity.y, inputValue.z * playerSpeed);
         }
         else
@@ -99,7 +136,7 @@ public class PlayerMovement : MonoBehaviour
         if (hit.collider != null)
         {
             float angle = Vector3.Angle(Vector3.up, hit.normal);
-            Debug.Log(angle);
+            //Debug.Log(angle);
             if (angle > maxAngleSlope)
             {
                 return false;
